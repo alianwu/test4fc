90e6df67f773c337556a42334cce9b1e5e0dff78
wxg4dev <wxg4dev@gmail.com>
2014-02-21 08:30:49 +0800

    更新仓库结构

--- application/views/manager/house/house_add_success.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');?>
-保存成功 。
-<?php echo HTML::anchor('manager_house_home/'. URL::query(array('hid'=>$hid)), '返回列表'); ?>  
-<?php echo HTML::anchor('manager_house_home/add', '继续添加'); ?>  
-<?php echo HTML::anchor('house_home/detail/'.$hid, '前台预览'); ?> 
-<?php echo HTML::anchor('manager_house_home/attachment'. URL::query(array('hid'=>$hid)), '管理或者上传附件'); ?>
