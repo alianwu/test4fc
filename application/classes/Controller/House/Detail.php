8073745e012dbc8cba6e7e4e8ab6773205c32643
wxg4dev <wxg4dev@gmail.com>
2014-03-20 09:44:58 +0800

    前台预览

--- application/classes/Controller/House/Detail.php
+++ application/classes/Controller/House/Detail.php
@@ -1,17 +1,24 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_House_Detail extends Controller_Template {
-  
+
+  public $manager; 
   public function before()
   {
     parent::before();
+    $this->manager = $this->get_user('manager.user');
     $this->model = Model::factory('House_New');
   }
 
   public function action_display()
   {
     $hid = (int) $this->request->param('id');
-    $data = $this->model->get_one_front($hid);
+    if ($this->manager) {
+      $data = $this->model->get_one($hid);
+    }
+    else {
+      $data = $this->model->get_one_front($hid);
+    }
     if ($data == NULL) {
       throw new Kohana_HTTP_Exception_404();
     }
