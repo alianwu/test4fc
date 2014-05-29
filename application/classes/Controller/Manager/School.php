42f47b87be74665b9e8b2dc07bf1703c501d377d
wxg4dev <wxg4dev@gmail.com>
2014-05-30 06:46:08 +0800

    取消xss检查

--- application/classes/Controller/Manager/School.php
+++ application/classes/Controller/Manager/School.php
@@ -92,7 +92,6 @@ class Controller_Manager_School extends Controller_Manager_Template {
       $post->rules($k, $v);
     }
     if($post->check()) {
-      $data = Security::xss_clean($post->data());
       $ret = $this->model->save_one($data);
       $view = View::factory('manager/school/school_editor_success')->set('ret', $ret);
       return $this->view($view);
