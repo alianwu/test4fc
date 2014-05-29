168e03e3cf9bb9820aec3777ba84918d907176c1
wxg4dev <wxg4dev@gmail.com>
2014-05-30 07:03:36 +0800

    修复房源地图显示

--- application/classes/Controller/House/Map.php
+++ application/classes/Controller/House/Map.php
@@ -2,12 +2,10 @@
 
 class Controller_House_map extends Controller_Template {
 
-  public $auto_render = FALSE;
   public function before()
   {
     parent::before();
   }
-
 
   public function action_index()
   {
@@ -16,7 +14,7 @@ class Controller_House_map extends Controller_Template {
     $view->bind_global('city_lat', $this->city_lat);
     $view->bind_global('city_lng', $this->city_lng);
     $view->bind_global('core', $this->core);
-    $this->response->body($view);
+    $this->view($view);
   }
 
 }
